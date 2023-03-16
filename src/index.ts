3e7d665d97a3c174d9967b1d69e351fe27ae12bf
Tobey Blaber <contact@tobey.me>
2023-03-16 13:50:44 +0000

    Added event system API: createPublisher, subscribe, unsubscribe, etc.

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,8 @@ import { EventEmitter } from 'events';
 import { createServer, Server, Socket, connect, ListenOptions } from 'net';
 import { EOL } from 'os';
 import { join } from 'path';
+import { Observable, fromEventPattern } from 'rxjs';
+import { first } from 'rxjs/operators';
 import TypedEmitter from 'typed-emitter';
 import { v4 as uuidv4 } from 'uuid';
 import { z } from 'zod';
@@ -74,6 +76,11 @@ const winRoot = join('\\\\.', 'pipe');
  * The root namespace for npc endpoints: `\\.\pipe\.npc`
  */
 export const rootNamespace = join(winRoot, '.npc');
+
+/**
+ * The namespace for npc events: `\\.\pipe\.npc\.events`
+ */
+export const eventsNamespace = join(rootNamespace, '.events');
 
 /**
  * {@link https://zod.dev Zod schema} for npc client requests
@@ -651,3 +658,443 @@ export async function notify(
         socket.destroy();
     }
 }
+
+/**
+ * A simple implementation of an npc event publisher.
+ */
+export interface NpcPublisher {
+    /**
+     * The endpoint at which the {@link NpcPublisher npc publisher} is available to be {@link subscribe subscribed}.
+     */
+    endpoint?: string | undefined;
+    /**
+     * Stops the {@link NpcPublisher npc publisher} from accepting new connections and closes it when all clients have disconnected.
+     * @param {boolean} [gracefulDisconnect=true] Whether existing connections should be maintained until they are ended.
+     * Passing `false` will immediately disconnect all connected clients. Defaults to `true`.
+     * @returns {Promise<NpcPublisher>} A {@link !Promise Promise} which when resolved indicates the underlying
+     * {@link node!net.Server Node.js Server} has closed.
+     */
+    close(gracefulDisconnect?: boolean): Promise<NpcPublisher>;
+    /**
+     * Publishes an event to all connected subscribers.
+     * @param {string} eventName The name of the event to publish.
+     * @param {unknown} [data] The data to publish with the event.
+     */
+    publish(eventName: string, data?: unknown): Promise<void>;
+    /**
+     * Starts the {@link NpcPublisher npc publisher} listening for client connections.
+     * @remarks The {@link endpoint} will be prefixed with the {@link rootNamespace root npc namespace}.
+     * @param {string} endpoint The endpoint at which an {@link NpcPublisher npc publisher} will be available to be {@link subscribe subscribed}.
+     * @param {Omit<ListenOptions, 'path' | 'port' | 'host' | 'ipv6Only'} [options] {@link @types/node!ListenOptions Options}
+     * to pass to the underlying {@link node!net.Server Node.js Server}.
+     * @returns {Promise<NpcPublisher>} A {@link !Promise Promise} which when resolved indicates the {@link NpcPublisher npc publisher} is ready to receive client connections.
+     */
+    listen(endpoint: string, options?: NpcListenOptions): Promise<NpcPublisher>;
+}
+
+/**
+ * {@link https://zod.dev Zod schema} for {@link NpcEvent event messages}.
+ * @see {@link https://zod.dev Zod}
+ */
+export const npcEventSchema = z.object({
+    npc: z.literal('0.2'),
+    eventName: z.string(),
+    data: z.unknown().optional(),
+});
+
+/**
+ * An event message, inferred from {@link npcEventSchema its schema}.
+ */
+export type NpcEvent = z.infer<typeof npcEventSchema>;
+
+/**
+ * Initializes a new {@link NpcPublisher npc event publisher}.
+ * @returns {NpcPublisher} A new {@link NpcPublisher npc event publisher}.
+ */
+export const createPublisher = () => {
+    const sockets = new Set<Socket>();
+    let server: Server | undefined;
+
+    const publisher: NpcPublisher = {
+        close: async (gracefulDisconnect = true) => {
+            const closing = server?.listening
+                ? new Promise<void>((resolve) => {
+                      server?.close(() => {
+                          server = undefined;
+                          resolve();
+                      });
+                  })
+                : Promise.resolve();
+
+            if (!gracefulDisconnect) {
+                for (const socket of sockets) {
+                    socket?.destroy();
+                }
+            }
+            await closing;
+            if (gracefulDisconnect) {
+                for (const socket of sockets.values()) {
+                    socket?.destroy();
+                }
+            }
+            sockets.clear();
+            delete publisher.endpoint;
+            return publisher;
+        },
+        listen: async (endpoint: string, options?: NpcListenOptions) => {
+            await publisher.close();
+
+            return new Promise<NpcPublisher>((resolve, reject) => {
+                server = createServer((socket) => {
+                    sockets.add(socket);
+                    socket
+                        .once('close', () => sockets.delete(socket))
+                        .once('error', () => sockets.delete(socket));
+                })
+                    .once('error', (error) => reject(error))
+                    .listen(
+                        {
+                            ...options,
+                            path: join(eventsNamespace, endpoint),
+                        },
+                        () => {
+                            publisher.endpoint = endpoint;
+                            resolve(publisher);
+                        }
+                    );
+            });
+        },
+        publish: async (eventName: string, data?: unknown) => {
+            const encoding = 'utf8';
+            const event: NpcEvent = {
+                npc: '0.2',
+                eventName,
+                data,
+            };
+            const stringified = `${JSON.stringify(event)}${EOL}`;
+
+            await Promise.all(
+                [...sockets]
+                    .filter((socket) => socket.writable)
+                    .map(
+                        (socket) =>
+                            new Promise<void>((resolve) =>
+                                socket.write(stringified, encoding, () =>
+                                    resolve()
+                                )
+                            )
+                    )
+            );
+        },
+    };
+    return publisher;
+};
+
+/**
+ * {@link https://zod.dev Zod schema} for qualified event names, e.g. `my-event@my-namespace`.
+ * @see {@link https://zod.dev Zod}
+ */
+export const qualifiedEventNameSchema = z.string().regex(/(.+)@(.+)/g);
+
+/**
+ * A qualified event name, e.g. `my-event@my-namespace`.
+ */
+export type QualifiedEventName = `${string}@${string}`;
+
+let subscriptionMap:
+    | Map<
+          string,
+          {
+              socket: Promise<Socket>;
+              subscriptions: Map<string, Set<(data: unknown) => void>>;
+          }
+      >
+    | undefined;
+
+const getSubscriptionMap = () =>
+    subscriptionMap ?? (subscriptionMap = new Map());
+
+/**
+ * Subscribe to an event published by an {@link NpcPublisher npc publisher}.
+ * @param {QualifiedEventName} qualifiedEventName The qualified event name, e.g. `my-event@my-namespace`.
+ * @param {(data: unknown) => void} subscriber The callback to invoke when the event is published.
+ */
+export function subscribe(
+    qualifiedEventName: QualifiedEventName,
+    subscriber: (data: unknown) => void
+): void;
+/**
+ * Subscribe to an event published by an {@link NpcPublisher npc publisher}.
+ * @param {string} endpoint The endpoint at which an {@link NpcPublisher npc publisher} is available to be {@link subscribe subscribed}.
+ * @param {string} eventName The name of the event to subscribe to.
+ * @param {(data: unknown) => void} subscriber The callback to invoke when the event is published.
+ */
+export function subscribe(
+    endpoint: string,
+    eventName: string,
+    subscriber: (data: unknown) => void
+): void;
+export function subscribe(
+    qualifiedEventNameOrEndpoint: QualifiedEventName | string,
+    subscriberOrEventName: ((data: unknown) => void) | string,
+    subscriber?: (data: unknown) => void
+) {
+    const hasSeparateEventName = z
+        .string()
+        .safeParse(subscriberOrEventName).success;
+
+    const eventName = hasSeparateEventName
+        ? z.string().parse(subscriberOrEventName)
+        : qualifiedEventNameSchema
+              .parse(qualifiedEventNameOrEndpoint)
+              .split('@')[0];
+    const endpoint = hasSeparateEventName
+        ? z.string().parse(qualifiedEventNameOrEndpoint)
+        : qualifiedEventNameSchema
+              .parse(qualifiedEventNameOrEndpoint)
+              .split('@')[1];
+    const callback = (
+        hasSeparateEventName ? subscriber : subscriberOrEventName
+    ) as (data: unknown) => void;
+
+    let map = getSubscriptionMap().get(endpoint);
+    if (!map) {
+        map = {
+            socket: new Promise<Socket>((resolve, reject) => {
+                try {
+                    const socket = connect(
+                        join(eventsNamespace, endpoint),
+                        () => {
+                            resolve(socket);
+                        }
+                    )
+                        .on('data', (buffer) => {
+                            for (const line of buffer
+                                .toString('utf8')
+                                .split(EOL)
+                                .filter(Boolean)) {
+                                const data = JSON.parse(line);
+                                const event = npcEventSchema.parse(data);
+                                for (const subscriber of getSubscriptionMap()
+                                    .get(endpoint)
+                                    ?.subscriptions.get(event.eventName) ||
+                                    []) {
+                                    subscriber?.(event.data);
+                                }
+                            }
+                        })
+                        .once('close', () =>
+                            getSubscriptionMap().delete(endpoint)
+                        )
+                        .once('error', () =>
+                            getSubscriptionMap().delete(endpoint)
+                        );
+                } catch (e) {
+                    reject(e);
+                }
+            }),
+            subscriptions: new Map<string, Set<(data: unknown) => void>>(),
+        };
+        getSubscriptionMap().set(endpoint, map);
+    }
+
+    if (map.subscriptions.has(eventName)) {
+        map.subscriptions.get(eventName)?.add(callback);
+    } else {
+        map.subscriptions.set(eventName, new Set([callback]));
+    }
+}
+export const on = subscribe;
+
+/**
+ * Unsubscribe from an event published by an {@link NpcPublisher npc publisher}.
+ * @remarks Cannot unsubscribe subscriptions which were subscribed from a different module.
+ * @param {QualifiedEventName} qualifiedEventName The qualified event name, e.g. `my-event@my-namespace`.
+ * @param {(data: unknown) => void} subscriber The callback to unsubscribe.
+ */
+export function unsubscribe(
+    qualifiedEventName: QualifiedEventName,
+    subscriber: (data: unknown) => void
+): void;
+/**
+ * Unsubscribe from an event published by an {@link NpcPublisher npc publisher}.
+ * @remarks Cannot unsubscribe subscriptions which were subscribed from a different module.
+ * @param {string} endpoint The endpoint at which an {@link NpcPublisher npc publisher} is available to be {@link subscribe subscribed}.
+ * @param {string} eventName The name of the event to unsubscribe from.
+ * @param {(data: unknown) => void} subscriber The callback to unsubscribe.
+ */
+export function unsubscribe(
+    endpoint: string,
+    eventName: string,
+    subscriber: (data: unknown) => void
+): void;
+export function unsubscribe(
+    qualifiedEventNameOrEndpoint: QualifiedEventName | string,
+    subscriberOrEventName: ((data: unknown) => void) | string,
+    subscriber?: (data: unknown) => void
+) {
+    if (!subscriptionMap) return;
+
+    const hasSeparateEventName = z
+        .string()
+        .safeParse(subscriberOrEventName).success;
+
+    const eventName = hasSeparateEventName
+        ? z.string().parse(subscriberOrEventName)
+        : qualifiedEventNameSchema
+              .parse(qualifiedEventNameOrEndpoint)
+              .split('@')[0];
+    const endpoint = hasSeparateEventName
+        ? z.string().parse(qualifiedEventNameOrEndpoint)
+        : qualifiedEventNameSchema
+              .parse(qualifiedEventNameOrEndpoint)
+              .split('@')[1];
+    const callback = (
+        hasSeparateEventName ? subscriber : subscriberOrEventName
+    ) as (data: unknown) => void;
+
+    const map = subscriptionMap.get(endpoint);
+    const subscriptions = map?.subscriptions.get(eventName);
+    subscriptions?.delete(callback);
+    if (subscriptions?.size === 0) {
+        map?.subscriptions.delete(eventName);
+    }
+    if (map?.subscriptions.size === 0) {
+        map.socket?.then((socket) => socket.destroy());
+        subscriptionMap.delete(endpoint);
+    }
+}
+export const off = unsubscribe;
+
+/**
+ * Unsubscribe from all {@link NpcPublisher npc publisher} events subscribed to by this module.
+ * @remarks Cannot unsubscribe subscriptions which were subscribed from a different module.
+ */
+export function unsubscribeAll(): void;
+/**
+ * Unsubscribe from all events at a given endpoint published by an {@link NpcPublisher npc publisher}.
+ * @remarks Cannot unsubscribe subscriptions which were subscribed from a different module.
+ * @param {string} endpoint The endpoint at which an {@link NpcPublisher npc publisher} is available to be {@link subscribe subscribed}.
+ * All subscriptions to all events at this endpoint will be unsubscribed.
+ */
+export function unsubscribeAll(endpoint: string): void;
+/**
+ * Unsubscribe all subscribers from a given event published by an {@link NpcPublisher npc publisher}.
+ * @remarks Cannot unsubscribe subscriptions which were subscribed from a different module.
+ * @param {string} endpoint The endpoint at which an {@link NpcPublisher npc publisher} is available to be {@link subscribe subscribed}.
+ * @param {string} eventName The name of the event to unsubscribe from. All subscriptions to this event will be unsubscribed.
+ */
+export function unsubscribeAll(endpoint: string, eventName: string): void;
+export function unsubscribeAll(endpoint?: string, eventName?: string) {
+    if (!subscriptionMap) return;
+
+    if (endpoint && eventName) {
+        subscriptionMap.get(endpoint)?.subscriptions.delete(eventName);
+        if (subscriptionMap.get(endpoint)?.subscriptions.size === 0) {
+            subscriptionMap
+                .get(endpoint)
+                ?.socket.then((socket) => socket.destroy());
+            subscriptionMap.delete(endpoint);
+        }
+    } else if (endpoint) {
+        subscriptionMap
+            .get(endpoint)
+            ?.socket.then((socket) => socket.destroy());
+        subscriptionMap.delete(endpoint);
+    } else {
+        for (const map of subscriptionMap.values()) {
+            map.socket.then((socket) => socket.destroy());
+        }
+        subscriptionMap.clear();
+    }
+}
+
+/**
+ * Create an observable from an {@link NpcPublisher npc publisher} event.
+ * @param {QualifiedEventName} qualifiedEventName The qualified event name, e.g. `my-event@my-namespace`.
+ * @returns {Observable<unknown>} An observable which emits the event data.
+ */
+export function createObservable(
+    qualifiedEventName: QualifiedEventName
+): Observable<unknown>;
+/**
+ * Create an observable from an {@link NpcPublisher npc publisher} event.
+ * @param {string} endpoint The endpoint at which an {@link NpcPublisher npc publisher} is available to be {@link subscribe subscribed}.
+ * @param {string} eventName The name of the event to subscribe to.
+ * @returns {Observable<unknown>} An observable which emits the event data.
+ */
+export function createObservable(
+    endpoint: string,
+    eventName: string
+): Observable<unknown>;
+export function createObservable(
+    qualifiedEventNameOrEndpoint: QualifiedEventName | string,
+    eventName?: string
+) {
+    const hasSeparateEventName = z.string().safeParse(eventName).success;
+
+    const endpoint = hasSeparateEventName
+        ? z.string().parse(qualifiedEventNameOrEndpoint)
+        : qualifiedEventNameSchema
+              .parse(qualifiedEventNameOrEndpoint)
+              .split('@')[1];
+    const event = hasSeparateEventName
+        ? z.string().parse(eventName)
+        : qualifiedEventNameSchema
+              .parse(qualifiedEventNameOrEndpoint)
+              .split('@')[0];
+
+    return fromEventPattern(
+        (handler) => on(endpoint, event, handler),
+        (handler) => off(endpoint, event, handler)
+    );
+}
+
+/**
+ * Register a one-time subscription to an event published by an {@link NpcPublisher npc publisher}.
+ * The next time the event is published, the subscriber will be unsubscribed and then invoked.
+ * @param {QualifiedEventName} qualifiedEventName The qualified event name, e.g. `my-event@my-namespace`.
+ * @param {(data: unknown) => void} subscriber The callback to invoke when the event is published.
+ */
+export function subscribeOnce(
+    qualifiedEventName: QualifiedEventName,
+    subscriber: (data: unknown) => void
+): void;
+/**
+ * Register a one-time subscription to an event published by an {@link NpcPublisher npc publisher}.
+ * The next time the event is published, the subscriber will be unsubscribed and then invoked.
+ * @param {string} endpoint The endpoint at which an {@link NpcPublisher npc publisher} is available to be {@link subscribe subscribed}.
+ * @param {string} eventName The name of the event to subscribe to.
+ * @param {(data: unknown) => void} subscriber The callback to invoke when the event is published.
+ */
+export function subscribeOnce(
+    endpoint: string,
+    eventName: string,
+    subscriber: (data: unknown) => void
+): void;
+export function subscribeOnce(
+    qualifiedEventNameOrEndpoint: QualifiedEventName | string,
+    subscriberOrEventName: ((data: unknown) => void) | string,
+    subscriber?: (data: unknown) => void
+) {
+    const hasSeparateEventName = z
+        .string()
+        .safeParse(subscriberOrEventName).success;
+
+    const eventName = hasSeparateEventName
+        ? z.string().parse(subscriberOrEventName)
+        : qualifiedEventNameSchema
+              .parse(qualifiedEventNameOrEndpoint)
+              .split('@')[0];
+    const endpoint = hasSeparateEventName
+        ? z.string().parse(qualifiedEventNameOrEndpoint)
+        : qualifiedEventNameSchema
+              .parse(qualifiedEventNameOrEndpoint)
+              .split('@')[1];
+    const callback = (
+        hasSeparateEventName ? subscriber : subscriberOrEventName
+    ) as (data: unknown) => void;
+
+    createObservable(endpoint, eventName).pipe(first()).subscribe(callback);
+}
+export const once = subscribeOnce;
